ff595614ba81a0c4540a4fba8149cf7599aa2242
Oliver Mathiesen <mathiesenoliver@gmail.com>
2021-11-02 13:21:24 +0100

    init

---
+++ core/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="primary">Bootstrap btn</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
