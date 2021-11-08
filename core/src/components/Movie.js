41ea84efd0063623c3c42b89c3b9f7d42098a2f6
Oliver Mathiesen <mathiesenoliver@gmail.com>
2021-11-08 09:32:38 +0100

    Ratings and Title

--- core/src/components/Movie.js
+++ core/src/components/Movie.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+// desired thumbnail dimensions
+const IMG_API = "orig-93x165";
+const RATING = 1;
 
-const IMG_API = "orig-93x165"
+const Movie = ({plprogram$titleLocalized, plprogram$thumbnails, description, plprogram$ratings}) => {
+    // try to grab movie thumbnail url. Default to null if desired dimensions is not defined
+    const thumbnail = plprogram$thumbnails[IMG_API] 
+    ? plprogram$thumbnails[IMG_API]['plprogram$url'] 
+    : null;
 
-const Movie = ({plprogram$titleLocalized, plprogram$thumbnails, description, plprogram$ratings, entries, plprogram$url}) =>  
-    <div className="movie">
-        <img src={plprogram$url} alt={plprogram$titleLocalized}/>
+
+    return <div className="movie">
+        <img src={thumbnail} alt={plprogram$titleLocalized.da}/>
+        <div className="movie-info">
+            <h3>{plprogram$titleLocalized.da}</h3>
+            <span>{entries[18].plprogram$ratings[0]}</span>
+        </div>
     </div>;
+};
 
-    export default Movie;
-   
+export default Movie;
