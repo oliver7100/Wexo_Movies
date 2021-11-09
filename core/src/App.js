fee002cd445d32e05be9d75fc1609d4274009f24
Oliver Mathiesen <mathiesenoliver@gmail.com>
2021-11-09 15:28:16 +0100

    pushing tags in different genres array.

--- core/src/App.js
+++ core/src/App.js
@@ -9,6 +9,18 @@ const SEARCH_API = "curl --location --request GET 'https://feed.entertainment.tv
 
 function App() {
   const [movies, setMovies] = useState([]); 
+  const [moviesGenre, setMoviesGenre] = useState({
+  Horror: [],
+  Action:[],
+  Comedy:[],
+  Thriller:[],
+  War:[],
+  Romance:[],
+  Drama:[],
+  Crime:[],
+  Documentary:[]
+  });
+
 
   useEffect(() => {
     fetch(FEATURED_API)
@@ -19,6 +31,26 @@ function App() {
     });
   },[]);
 
+
+  
+  function sortGenres(movie){
+    movie["plprogram$tags"].map(tag=>{
+      console.log(tag['plprogram$title']) 
+      if(tag['plprogram$title'] in ['plprogram$tags'] )
+      {
+        moviesGenre[tag['plprogram$title']].push(movie);
+
+      }
+      {
+
+      }
+
+
+
+      console.log(moviesGenre);
+    })
+  }
+  movies.map(sortGenres);
   
   return movies.map((movie, index) => 
     <Movie key={index} {...movie}/>
--- core/src/components/Movie.js
+++ core/src/components/Movie.js
@@ -4,20 +4,33 @@ import React from "react";
 const IMG_API = "orig-93x165";
 const RATING = 1;
 
-const Movie = ({plprogram$titleLocalized, plprogram$thumbnails, description, plprogram$ratings}) => {
+const Movie = ({plprogram$titleLocalized, plprogram$thumbnails, description, title, guid, plprogram$ratings}) => {
     // try to grab movie thumbnail url. Default to null if desired dimensions is not defined
+
+      //Normal if statement, but for more readable purposes I made an if statement for a better example. 
     const thumbnail = plprogram$thumbnails[IMG_API] 
     ? plprogram$thumbnails[IMG_API]['plprogram$url'] 
     : null;
 
+    console.log(plprogram$ratings.length);
+    const movieRatings = plprogram$ratings.length > 0
+    ?  plprogram$ratings[0]['plprogram$rating'] : "This Movie hasn't been rated yet." ;
+    
+    
+    let movieTitle = title;
+
+    if (plprogram$titleLocalized.da)
+    {
+        movieTitle = plprogram$titleLocalized.da
+    }
 
     return <div className="movie">
-        <img src={thumbnail} alt={plprogram$titleLocalized.da}/>
+        <img src={thumbnail} alt={movieTitle}/>
         <div className="movie-info">
-            <h3>{plprogram$titleLocalized.da}</h3>
-            <span>{entries[18].plprogram$ratings[0]}</span>
+            <h3>{movieTitle}</h3>
+          <p>{movieRatings}</p>
         </div>
-    </div>;
+    </div>
 };
 
 export default Movie;
